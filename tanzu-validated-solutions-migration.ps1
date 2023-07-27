81e2af80271f92037a8a3e189b08400e1a660316
Olaf Gradin <olaf@gradin.com>
2023-07-27 08:54:55 -0400

    Temporary doc migrator

---
+++ tanzu-validated-solutions-migration.ps1
@@ -0,0 +1,55 @@
+[CmdletBinding()]
+param (
+    [Parameter(Position=1)]
+    [string]
+    $Directory="."
+)
+
+$DirList = Get-ChildItem -Path "$($Directory)/*" -Include *.md -Exclude index*
+
+$images = "images"
+$index = "index.md"
+$section = "refarchs"
+$tmp = $env:TEMP
+$resource = "resources"
+
+if ( -Not (Test-Path "$($tmp)/$($section)")) {
+    New-Item -Path "$($tmp)" -ItemType Directory -Name $section | out-null
+}
+foreach($file in $DirList) {
+    $contents = Get-Content $file
+
+    foreach($line in $contents) {
+        if ($line -match '[^!]\[[\w\s\d]+\]\(([./]+[\w\d./?=-]+)\)') {
+            if ( -Not (Test-Path "$($tmp)/$($section)/$($bundle)/$($resource)")) {
+                New-Item -Path "$($tmp)/$($section)/$($bundle)" -ItemType Directory -Name $resource | out-null
+            }
+            $warn += @("Possible relative link issue ($($file.name)): $($Matches[1])")
+        }
+
+        if ($line -match '[^!]\[[\w\s\d]+\]\(([./]+[\w\d./?=-]+\.[a-z]+)\)') {
+            Copy-Item -Path "$($Directory)/$($Matches[2])" -Destination "$($tmp)/$($section)/$($bundle)/$($images)/$($Matches[3])" -Force
+        }
+
+        if ($line -match '(!\[[\w\s\d]+\])\(([\w\d./?=#-]*img\/[\w\d./?=#-]+\/([\w\d./?=#-]+\.[a-z]{3}))\)') {
+            Copy-Item -Path "$($Directory)/$($Matches[2])" -Destination "$($tmp)/$($section)/$($bundle)/$($images)/$($Matches[3])" -Force
+        }
+    }
+
+    $contents = $contents -replace '(!\[[\w\s\d]+\])\([\w\d./?=#-]*img\/[\w\d./?=#-]+\/([\w\d./?=#-]+\.[a-z]{3})\)','$1(images/$2)'
+    $bundle =  ($file.name).substring(0, ($file.name).length -3)
+
+    if ( -Not (Test-Path "$($tmp)/$($section)/$($bundle)")) {
+        New-Item -Path "$($tmp)/$($section)" -ItemType Directory -Name $bundle | out-null
+    }
+    
+    if ( -Not (Test-Path "$($tmp)/$($section)/$($bundle)/$($images)")) {
+        New-Item -Path "$($tmp)/$($section)/$($bundle)" -ItemType Directory -Name $images | out-null
+    }
+
+    Set-Content -Path "$($tmp)/$($section)/$($bundle)/$($index)" -Force -Value $contents
+    $info += "File $($_.name) migrated to $($tmp)/$($section)/$($bundle)"
+}
+
+$info | Write-Information
+$warn | Write-Warning
